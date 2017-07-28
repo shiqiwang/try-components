39ce743cd98d10afa8ec920c3f10935cb54cfd18
shiqiwang <shiqi.wang@live.com>
2017-07-28 14:00:24 +0800

    try

---
+++ App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">-->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+  @font-face {
+    font-family: 'iconfont';
+    src: url('./assets/selectIconFont/iconfont.eot');
+    src: url('./assets/selectIconFont/iconfont.eot?#iefix') format('embedded-opentype'),
+    url('./assets/selectIconFont/iconfont.woff') format('woff'),
+    url('./assets/selectIconFont/iconfont.ttf') format('truetype'),
+    url('./assets/selectIconFont/iconfont.svg#iconfont') format('svg');
+  }
+#app {
+  font-family: 'Microsoft YaHei','Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ components/pullDownList.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="pull-down-list">
+    <div v-for="item in items">
+      <span class="selectImg">&#xe501</span>
+      <span v-bind:data-id="item.id" class="personName">{{item.name}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'pull-down-list',
+    data () {
+      return {
+        items: [
+          {id: 0, name: 'wang'},
+          {id: 1, name: 'shi'},
+          {id: 2, name: 'qi'}
+        ],
+        isSelected: false
+      }
+    }
+//    methods: {
+//      classChange: function () {
+//        if(this.isSelected) {
+//
+//        }
+//      }
+//    }
+  }
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+  }
+
+  #pull-down-list {
+    border: 1px #ccc solid;
+    box-shadow: aliceblue;
+    border-radius: 4px;
+    width: 120px;
+  }
+
+  #pull-down-list div {
+    height: 40px;
+  }
+
+  #pull-down-list div:hover {
+    cursor: pointer;
+  }
+
+  #pull-down-list div .selectImg {
+    font-family:iconfont;
+    font-size: 18px;
+    color: #ccc;
+    float: left;
+    height: 40px;
+    width: 40px;
+    line-height:40px;
+  }
+
+  #pull-down-list div .personName {
+    line-height: 40px;
+    display: inline-block;
+    color: #aaa;
+    height: 40px;
+  }
+</style>
